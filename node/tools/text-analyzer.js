6ce55368286a96533d03cbe0a7fd04e210b5960e
Willem Melder <wmelder@beeldengeluid.nl>
2014-09-15 13:57:25 +0200

    Added IDF file and changed getMostImportantWords

--- node/tools/text-analyzer.js
+++ node/tools/text-analyzer.js
@@ -18,7 +18,7 @@ module.exports = {
 		}
 		return stopWords;
 	},
-	
+	/*
 	readIDFFile : function(IDFFile) {
 		if (!IDFFile) {
 			return null;
@@ -35,7 +35,25 @@ module.exports = {
 		}
 		return wordFreqs;
 	},
-		
+	*/
+	
+	readIDFFile : function(IDFFile) {
+		if (!IDFFile) {
+			return null;
+		}
+		var fileData = fs.readFileSync(IDFFile, 'utf8');
+		var idf = {};
+		var data_arr = fileData.split('\n');
+		var line = null;
+		for (key in data_arr) {
+			line = data_arr[key].split('\t');
+			if(line && line[0] && line[2]) {
+				idf[line[0].trim()] = parseFloat(line[2].trim());
+			}
+		}
+		return idf;
+	},
+				
 	getMostImportantWords : function(text, stopWords, IDFScores, isCleanText, minwordlength, simpleList) {		
 	    if(text == null || text == '') {
 	        return null;        
@@ -55,25 +73,22 @@ module.exports = {
 	    //split up the text in separate words and calculate the frequency of each word (+ the total number of words)
 	    var word_arr = text.split(' ');	    
 	    
+		/*
 	    //for tf-idf calculations
 	    if(this.wordsGrandTotal == -1) {
 	    	for(key in IDFScores) {
 	    		this.wordsGrandTotal += IDFScores[key];
 	    	}
 	    }
-	    var thisDocGrandTotal = word_arr.length;	   
-        var docCount = parseFloat(this.wordsGrandTotal/thisDocGrandTotal)
-	    
-	    var wordID = null;
+		*/
+		
+	    var thisDocGrandTotal = word_arr.length;
 	    for(key in word_arr) {
-	        totalWordCount += 1;
-	        wordID = word_arr[key];//.toLowerCase();
-	        
 	        //collect overall word statistics
-	        if (!allwords[wordID]) {
-	            allwords[wordID] = 1
+	        if (!allwords[key]) {
+	            allwords[key] = 1
 	        } else {
-	            allwords[wordID] += 1
+	            allwords[key] += 1
 	        }
 	    }
 	    
@@ -91,36 +106,35 @@ module.exports = {
 	        }
 	        
 	        //term frequency-inverse document frequency.
-	        var key_freq = allwords[wordID];
-	        var numDocsContaining = 1.0;
-	        if(IDFScores[key]) {	        	
-	        	numDocsContaining = (parseFloat(key_freq) / IDFScores[key]) * docCount;
-	        }
+	        var key_freq = allwords[key];
             var tf = parseFloat(key_freq) / thisDocGrandTotal;
-            var idf = Math.log(docCount/numDocsContaining);
-            var largedecision = tf * idf * key.length;
-            results.push([key, largedecision, key_freq]);
-            
-	        /*
-	        var tfidf = parseFloat(IDFScores[key]) / totalWordCount * 100;
-	        var freq = allwords[key];
-	        var largedecision = freq * tfidf * parseFloat(key.length);
-	        results.push([key, largedecision, freq]);*/
-	        
+            var idf = IDFScores[key]
+			var tfidf = tf*idf
+			
+			// [wm] var lengthRelevancy = (minwordlength - parseFloat(key.length)/minwordlength )) / minwordlength
+			
+            var largedecision = tfidf * key.length;
+            results.push([key, largedecision, key_freq]);        
 	    }
-	    
+
+	    //sort the list by highest score
+	    results_sorted = results.sort(function(a, b){return b-a});
+
+		/*
 	    //sort the list by highest score
 	    results_sorted = results.sort(function(a, b) {
 	    	if (a[1] && b[2]) {
 	    		return a[1] - b[1];
 	    	}
 	    });
+		*/
 	    
 	    //By default, just return the most frequent words in a simple list without scores
 	    if(simpleList && results_sorted) {
 	        var fws = [];
 	        for(key in results_sorted) {
-	            fws.push(results_sorted[key][0]);
+	            //fws.push(results_sorted[key][0]);
+				fws.push(key);
 	        }
 	        return fws;
 	    }
